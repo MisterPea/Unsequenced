9e8925e3381e28cfd352135c664e7ca94efa6344
Perry Angelora <peamaillist@gmail.com>
2022-07-17 22:27:34 -0400

    Added tests

--- unsequenced/__tests__/AppEntry.test.tsx
+++ unsequenced/__tests__/AppEntry.test.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, screen } from '@testing-library/react-native';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import AppEntry from '../AppEntry';
 
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
 describe('AppEntry.tsx', () => {
   it('Renders the component', () => {
-    render(<Provider store={store}><AppEntry /></Provider>);
+    render(
+      <Provider store={store}>
+        <AppEntry />
+      </Provider>,
+    );
+  });
+
+  it('Renders settings button', () => {
+    const component = <Provider store={store}><AppEntry /></Provider>;
+    const { getByTestId } = render(component);
+    const settingsBtn = getByTestId('taskBlockSettings');
+
+    expect(settingsBtn).toBeTruthy();
+  });
+
+  it('Takes you to settings page on button click', async () => {
+    const component = <Provider store={store}><AppEntry /></Provider>;
+    const { getByTestId } = render(component);
+    const settingsBtn = getByTestId('taskBlockSettings');
+
+    fireEvent(settingsBtn, 'press');
+    const settingsText = await screen.findByText('LIGHT/DARK MODE');
+
+    expect(settingsText).toBeTruthy();
   });
 });
--- unsequenced/__tests__/Settings.test.tsx
+++ unsequenced/__tests__/Settings.test.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
 import { Provider } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react-native';
 import store from '../redux/store';
 import Settings from '../screens/Settings';
 
-describe('Settings.tsx', () => {
+describe('Settings component', () => {
+  const component = (
+    <Provider store={store}>
+      <Settings />
+    </Provider>
+  );
+
   it('Renders the component', () => {
-    render(<Provider store={store}><Settings /></Provider>);
+    render(component);
+  });
+
+  // Screen Mode
+  it('screenMode.mode has initial value of "light"', () => {
+    const screenState = store.getState().screenMode.mode;
+    expect(screenState).toBe('light');
+  });
+
+  it('Toggles the mode from "light" to "dark"', () => {
+    const { getByTestId } = render(component);
+    const darkModeBtn = getByTestId('lightDarkModeBtn');
+
+    fireEvent(darkModeBtn, 'valueChange', 'true');
+
+    const screenState = store.getState().screenMode.mode;
+    expect(screenState).toBe('dark');
+  });
+
+  // Quiet Mode
+  it('quietMode has initial value of false', () => {
+    const quietState = store.getState().quietMode.isQuiet;
+    expect(quietState).toBeFalsy();
+  });
+
+  it('Toggles isQuiet mode from false to true', () => {
+    const { getByTestId } = render(component);
+    const isQuietBtn = getByTestId('quietModeBtn');
+
+    fireEvent(isQuietBtn, 'valueChange', 'true');
+
+    const quietState = store.getState().quietMode.isQuiet;
+    expect(quietState).toBeTruthy();
   });
 });
