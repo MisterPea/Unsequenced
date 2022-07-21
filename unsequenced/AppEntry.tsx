3d60202c9755ad515cca3b923a32ce39a1734f08
Perry Angelora <peamaillist@gmail.com>
2022-07-21 19:02:12 -0400

    Added nested Navigator

--- unsequenced/AppEntry.tsx
+++ unsequenced/AppEntry.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Pressable } from 'react-native';
 import TaskBlocks from './screens/TaskBlocks';
@@ -11,19 +12,15 @@ import NowPlaying from './screens/NowPlaying';
 import Settings from './screens/Settings';
 import { useAppSelector } from './redux/hooks';
 import colors from './constants/GlobalStyles';
-
-type RootStackParamList = {
-  Settings: undefined
-};
-type SettingsScreenProps = NativeStackScreenProps<RootStackParamList, 'Settings'>;
-type NavProps = SettingsScreenProps['navigation'];
+import CreateNewTaskBlock from './screens_modals/CreateNewTaskBlock';
+import { SettingsNavProps } from './constants/types';
 
 export default function AppEntry() {
   const { mode } = useAppSelector((state) => state.screenMode);
   const statusBarStyle = mode === 'dark' ? 'light' : 'dark';
   const Stack = createNativeStackNavigator();
 
-  function SettingsButton({ testID, nav }:{testID:string, nav:NavProps}) {
+  function SettingsButton({ testID, nav }:{testID:string, nav:SettingsNavProps}) {
     return (
       <Pressable
         testID={testID}
@@ -35,6 +32,30 @@ export default function AppEntry() {
           color={colors.textTwo[mode]}
         />
       </Pressable>
+    );
+  }
+
+  function TaskBlocksNavigator() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Task Blocks"
+          component={TaskBlocks}
+          options={({ navigation }) => ({
+            headerShown: false,
+            headerRight: () => <SettingsButton testID="taskBlockSettings" nav={navigation} />,
+          })}
+        />
+        <Stack.Screen
+          name="createNewTaskBlock"
+          initialParams={{ mode }}
+          component={CreateNewTaskBlock}
+          options={{
+            presentation: 'transparentModal',
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
     );
   }
 
@@ -54,17 +75,30 @@ export default function AppEntry() {
           }}
         >
           <Stack.Screen
-            name="Task Blocks"
-            component={TaskBlocks}
+            name="TaskBlocksNavigator"
+            component={TaskBlocksNavigator}
             options={({ navigation }) => ({
+              title: 'Task Blocks',
               headerRight: () => <SettingsButton testID="taskBlockSettings" nav={navigation} />,
             })}
           />
           <Stack.Screen
             name="Add a Task"
+            initialParams={{ mode }}
             component={AddTask}
             options={({ navigation }) => ({
               headerRight: () => <SettingsButton testID="addTaskSettings" nav={navigation} />,
+              headerLeft: () => (
+                <Pressable onPress={() => navigation.reset({ index: 1,
+                  routes: [{ name: 'TaskBlocksNavigator' }] })}
+                >
+                  <MaterialIcons
+                    name="arrow-back-ios"
+                    size={28}
+                    color={colors.textTwo[mode]}
+                  />
+                </Pressable>
+              ),
             })}
           />
           <Stack.Screen
@@ -74,6 +108,7 @@ export default function AppEntry() {
               headerRight: () => <SettingsButton testID="nowPlayingSettings" nav={navigation} />,
             })}
           />
+
           <Stack.Screen
             name="Settings"
             component={Settings}
